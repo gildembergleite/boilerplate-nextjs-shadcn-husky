ffe5e03173d87ec9f54d9f2fd93123587a3f6944
Gildemberg Leite <gildembergleite@gmail.com>
2024-03-14 08:45:42 -0300

    lint: corrections in files

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,8 +1,7 @@
-
 export default function HomePage() {
   return (
     <main className="flex w-screen h-screen items-center justify-center">
       <h1>Hello, World!</h1>
     </main>
-  );
+  )
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,21 +1,21 @@
-import { cn } from "@/lib/utils";
-import type { Metadata } from "next";
-import { Inter as FontSans } from "next/font/google";
-import { ReactNode } from "react";
-import "./globals.css";
+import { cn } from '@/lib/utils'
+import type { Metadata } from 'next'
+import { Inter as FontSans } from 'next/font/google'
+import { ReactNode } from 'react'
+import './globals.css'
 
 const fontSans = FontSans({
-  subsets: ["latin"],
-  variable: "--font-sans",
-});
+  subsets: ['latin'],
+  variable: '--font-sans',
+})
 
 export const metadata: Metadata = {
-  title: "Boilerplate NextJS",
-  description: "With shadcn/ui and husky configured",
-};
+  title: 'Boilerplate NextJS',
+  description: 'With shadcn/ui and husky configured',
+}
 
 interface RootLayoutProps {
-  children: ReactNode;
+  children: ReactNode
 }
 
 export default function RootLayout({ children }: Readonly<RootLayoutProps>) {
@@ -23,12 +23,12 @@ export default function RootLayout({ children }: Readonly<RootLayoutProps>) {
     <html lang="en">
       <body
         className={cn(
-          "min-h-screen bg-background font-sans antialiased",
-          fontSans.variable
+          'min-h-screen bg-background font-sans antialiased',
+          fontSans.variable,
         )}
       >
         {children}
       </body>
     </html>
-  );
+  )
 }
